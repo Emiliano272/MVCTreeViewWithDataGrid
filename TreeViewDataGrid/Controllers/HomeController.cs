4d38fedac30a58d29d4f5a5a2dfb669c383b6285
Emilian Skoczylas <emil272@vp.pl>
2019-10-24 16:27:08 +0200

    Importing project

---
+++ TreeViewDataGrid/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TreeViewDataGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Employee> list = new List<Employee>();
+            using (MyDBEntities dc = new MyDBEntities())
+            {
+                list = dc.Employees.OrderBy(a => a.EmployeeID).ToList();
+            }
+            return View(list);
+        }
+    }
+}
